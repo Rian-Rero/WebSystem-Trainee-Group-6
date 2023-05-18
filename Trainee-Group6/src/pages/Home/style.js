2532aab07144c4f7db163e065b68df3642802776
JPCopelli <joaocopelli@cpejr.com.br>
2023-05-18 20:11:41 -0300

    Fiz o quadro, perdi o .jsx na hora do commit

--- Trainee-Group6/src/pages/Home/style.js
+++ Trainee-Group6/src/pages/Home/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import PoppinsB from "../../font/Poppins-Bold.ttf";
 
 export const DivHome = styled.div`
+    position: relative;
     background-color: #2F2E6F;
     border: none;       
 
@@ -20,15 +21,27 @@ export const Imagem = styled.image`
 
 `
 export const Body = styled.div`
-    background-color: #2F2E6F;
-    margin-top: -21px;
-    margin-left: -8px;
-    height: 975px;
+background-color: #2F2E6F;
+width: 100%;
+height: 100vh;
+
+@media screen and (max-width: 1330px)
+{
+   
+}
+@media screen and (max-width: 1064)
+{
+   
+}
+@media screen and (max-width: 600) 
+{
+    
+}
 `;
 export const DivLogo = styled.div`
     background-color: #09082F;
     height: 129px;
-    width: 1900px;
+    width: 100%;
 `
 
 export const PageTitle = styled.image`
@@ -36,6 +49,7 @@ export const PageTitle = styled.image`
     top: 2px;
     right: 1170px;
     left: 270px;
+    
 `
 export const MenuLateral = styled.nav`
     position: fixed;
@@ -48,7 +62,7 @@ export const MenuLateral = styled.nav`
     overflow: hidden;
     transition: 0.3s;
     &:hover {
-    width: 245px;
+    width: 240px;
    
 }
 
@@ -79,22 +93,98 @@ export const Expandir = styled.div`
 
 export const DivRetangulo = styled.div`
 
-    margin-left: 70px;
+    
     text-align: center;
     font-style: normal;
-    font-weight: 400;
-    font-size: 45px;
+    font-weight: 600;
+    font-size: 20px;
     line-height: 65px;
     box-sizing: border-box;
-    position: absolute;
-    width: 1145px;
-    height: 75px;
-    left: 270px;
-    top: 199px;
+    position: relative;
+    width: 92%;
+    height: 60px;
+    left: 93px;
+    top: 600px;
     background: #9998C5;
-    border: 4px solid #FFFFFF;
-    border-radius: 45px;
-    color: #000000;
+    color: #2F2E6F;
 
 `
+
+
+export const DivQuadro = styled.div`
+
+    text-align: center;
+    font-style: normal;
+    font-weight: 250;
+    font-size: 35px;
+    line-height: 60px;
+    box-sizing: border-box;
+    width: 92%;
+    height: 1200px;
+    position: relative;
+    left: 93px;
+    top: 600px;
+    margin-bottom: 100px;
+    background-color: #9998C5;   
+`
+
+export const Tabela = styled.table`
+    box-sizing: border-box;
+    width: 90%;
+    height: 1200px;
+    position: relative;
+    left: 93px;
+    top: 600px;
+    background-color: #9998C5;
+    
+`
+export const TabelaHeader = styled.th`
+    text-align: left;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    color: #2F2E6F;
+    line-height: 80px;
+    position: relative;
+    margin-right: 150px;
+       
+    
+   
+
+`
+export const TabelaLinhaH = styled.tr` 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    border-bottom: 1px solid  #2F2E6F;
+
+
+`
+export const TabelaLinhaD = styled.tr` 
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+
+`
+export const TabelaCelula = styled.td` 
+    font-style: normal;
+    font-weight: 250;
+    font-size: 20px;
+    color: #2F2E6F;
+    line-height: 130px;
+    margin-right: 150px;
+   
+    
+    
+    
+    
+    
+    
+
+    
+
+
+
+`
+
 
